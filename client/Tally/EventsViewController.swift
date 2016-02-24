48bfaa9fbbd48de0d749e8910af0097ee5efd59d
Ryan Oldenburg <guzba8@gmail.com>
2016-02-24 01:49:51 -0800

    better

--- client/Tally/EventsViewController.swift
+++ client/Tally/EventsViewController.swift
@@ -4,7 +4,10 @@ import SDWebImage
 class EventsViewController : UITableViewController {
     private let segmentedControl = UISegmentedControl(items: ["Recent", "Top"])
     private let  activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
-    var events = [Event]()
+    private var events = [Sort : [Event]]()
+    private var activeSort: Sort {
+        return segmentedControl.selectedSegmentIndex == 0 ? .Recent : .Top
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +25,7 @@ class EventsViewController : UITableViewController {
         
         refreshControl = UIRefreshControl()
         refreshControl!.tintColor = Colors.purple
-        refreshControl!.addTarget(self, action: "refresh", forControlEvents: .ValueChanged)
+        refreshControl!.addTarget(self, action: "refresh:", forControlEvents: .ValueChanged)
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -30,9 +33,7 @@ class EventsViewController : UITableViewController {
         
         navigationItem.titleView = segmentedControl
         
-//        tableView.contentInset = UIEdgeInsetsMake(UIApplication.sharedApplication().statusBarFrame.height + navigationController!.navigationBar.frame.height, 0, tabBarController!.tabBar.frame.height, 0)
-//        
-        refresh()
+        refresh(self)
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -40,11 +41,11 @@ class EventsViewController : UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return events.count
+        return events[activeSort]?.count ?? 0
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let event = events[indexPath.row]
+        let event = events[activeSort]![indexPath.row]
         let cell = tableView.dequeueReusableCellWithIdentifier("EventCell", forIndexPath: indexPath) as! EventCell
         
         cell.headline.presentMarkdown(event.headline)
@@ -64,49 +65,53 @@ class EventsViewController : UITableViewController {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         
         let eventViewController = storyboard!.instantiateViewControllerWithIdentifier("StoryViewController") as! EventViewController
-        eventViewController.event = events[indexPath.row]
+        eventViewController.event = events[activeSort]![indexPath.row]
         navigationController!.pushViewController(eventViewController, animated: true)
     }
     
     func segmentIndexSelected(sender: UISegmentedControl) {
-        events.removeAll()
         tableView.reloadData()
         tableView.backgroundView = activityIndicator
         
-        refresh()
+        refresh(self)
     }
     
-    func refresh() {
-        Requests.get(Endpoints.events, completionHandler: { response, error in
+    func refresh(sender: AnyObject) {
+        if sender is EventsViewController && events[activeSort] != nil {
+            return
+        }
+        
+        let sort = activeSort
+        let url = Endpoints.events + "?sort=" + sort.rawValue
+        Requests.get(url, completionHandler: { response, error in
             self.refreshControl!.endRefreshing()
             
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(0.5 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
-                self.events.removeAll()
+                self.events[sort]?.removeAll()
                 
                 if response?.statusCode == 200 {
+                    self.events[sort] = [Event]()
                     if let stories = response!.body!["events"] as? [[String : AnyObject]] {
                         for story in stories {
                             do {
-                                self.events.append(try Event(data: story))
+                                self.events[sort]!.append(try Event(data: story))
                             } catch _ {
                                 p("Skipping invalid event")
                             }
                         }
                     }
+                    
+                    self.tableView.backgroundView = nil
+                } else {
+                    // Something bad happened
                 }
                 
                 self.tableView.reloadData()
-                
-                if (self.events.count == 0) {
-                    if response?.statusCode == 200 {
-                        // There are no events right now
-                    } else {
-                        // Something bad happened
-                    }
-                } else {
-                    self.tableView.backgroundView = nil
-                }
             }
         })
+    }
+    
+    enum Sort : String {
+        case Recent = "recent", Top = "top"
     }
 }
