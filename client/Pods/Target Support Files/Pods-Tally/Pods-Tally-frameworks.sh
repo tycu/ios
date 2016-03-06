b57dba1800a7a34ba25957eb5fe00b73827d3790
Ryan Oldenburg <guzba8@gmail.com>
2016-03-05 21:51:22 -0800

    progress

--- client/Pods/Target Support Files/Pods-Tally/Pods-Tally-frameworks.sh
+++ client/Pods/Target Support Files/Pods-Tally/Pods-Tally-frameworks.sh
@@ -84,6 +84,7 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-Tally/ActionSheetPicker_3_0.framework"
   install_framework "Pods-Tally/Bolts.framework"
   install_framework "Pods-Tally/FBSDKCoreKit.framework"
   install_framework "Pods-Tally/FBSDKLoginKit.framework"
@@ -94,6 +95,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-Tally/TTTAttributedLabel.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-Tally/ActionSheetPicker_3_0.framework"
   install_framework "Pods-Tally/Bolts.framework"
   install_framework "Pods-Tally/FBSDKCoreKit.framework"
   install_framework "Pods-Tally/FBSDKLoginKit.framework"
--- client/Tally/DonateViewController.swift
+++ client/Tally/DonateViewController.swift
@@ -1,13 +1,19 @@
+import ActionSheetPicker_3_0
+
 class DonateViewController : UIViewController {
     @IBOutlet weak var eventThumbnail: UIImageView!
     @IBOutlet weak var eventHeadline: MarkdownLabel!
     @IBOutlet weak var eventGraph: SupportOpposeView!
     @IBOutlet weak var eventTime: UILabel!
+    @IBOutlet weak var pacName: UILabel!
+    @IBOutlet weak var amount: UILabel!
     @IBOutlet weak var donate: UIButton!
     @IBOutlet weak var indicator: UIActivityIndicatorView!
     var event: Event!
     var pac: Pac!
     var inSupport: Bool!
+    
+    private var amounts = [3, 10, 25, 50, 100]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +33,10 @@ class DonateViewController : UIViewController {
         eventGraph.opposeTotal = event.opposeTotal
         eventTime.text = event.created.humanReadableTimeSinceNow
         
+        pacName.text = pac.name
+        
+        amount.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "changeAmount:"))
+        
         donate.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "donate:"))
     }
     
@@ -38,18 +48,17 @@ class DonateViewController : UIViewController {
         donate.setTitle(title, forState: .Normal)
     }
     
-    private func lockUI() {
-        navigationItem.leftBarButtonItem?.enabled = false
-        navigationItem.rightBarButtonItem?.enabled = false
-        donate.hidden = true
-        indicator.hidden = false
-    }
-    
-    private func unlockUI() {
-        navigationItem.leftBarButtonItem?.enabled = true
-        navigationItem.rightBarButtonItem?.enabled = true
-        donate.hidden = false
-        indicator.hidden = true
+    func changeAmount(sender: UITapGestureRecognizer) {
+        var amountStrings = [String]()
+        for amount in amounts {
+            amountStrings.append("$\(amount)")
+        }
+        
+        ActionSheetStringPicker.showPickerWithTitle("Select Amount", rows: amountStrings, initialSelection: 0, doneBlock: { picker, index, value in
+            
+            print("\(self.amounts[index])")
+            
+        }, cancelBlock: nil, origin: sender.view)
     }
     
     func donate(sender: AnyObject) {
@@ -66,6 +75,20 @@ class DonateViewController : UIViewController {
                 self.dismiss()
             })
         })
+    }
+    
+    private func lockUI() {
+        navigationItem.leftBarButtonItem?.enabled = false
+        navigationItem.rightBarButtonItem?.enabled = false
+        donate.hidden = true
+        indicator.hidden = false
+    }
+    
+    private func unlockUI() {
+        navigationItem.leftBarButtonItem?.enabled = true
+        navigationItem.rightBarButtonItem?.enabled = true
+        donate.hidden = false
+        indicator.hidden = true
     }
     
     func cancel() {
